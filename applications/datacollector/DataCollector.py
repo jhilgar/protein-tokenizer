28866b3193bab6d6742882abef14b2d88d2b1e86
Jeremy Hilgar <jeremyhilgar@live.com>
2024-02-26 15:06:54 -0800

    Python imports on Heroku

--- applications/datacollector/DataCollector.py
+++ applications/datacollector/DataCollector.py
@@ -1,9 +1,12 @@
 import io
 import re
+import time
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
 from Bio import SeqIO
+
+from components.database.databasehandler import DatabaseHandler
 
 # Code adapted from uniprot.org/help/api_queries
 class DataCollector():
@@ -31,3 +34,17 @@ class DataCollector():
         for batch, total in self.get_batch(batch_url):
             for record in SeqIO.parse(io.StringIO(batch.text), "fasta"):
                 yield record.id, record.seq
+
+if (__name__ == "__main__"):
+    collector = DataCollector()
+    handler = DatabaseHandler()
+
+    url = 'https://rest.uniprot.org/uniprotkb/search?format=fasta&query=%28Insulin+AND+%28reviewed%3Atrue%29+AND+%28organism_id%3A9823%29+AND+%28length%3A%5B350+TO+400%5D%29%29&size=500'
+
+    query_id = handler.insert_query(url)
+
+    for id, seq in collector.get_records(url):
+        handler.insert_dataset(query_id, str(seq))
+
+    while(True):
+        time.sleep(1)
